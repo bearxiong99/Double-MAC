a49320a80916e0eedf7a034a1df3101bc4a9178d
mp3jjk <jhjung@lanada.kaist.ac.kr>
2017-05-24 20:32:56 +0900

    data ack debugging

--- core/net/mac/cxmac/cxmac.c
+++ core/net/mac/cxmac/cxmac.c
@@ -186,7 +186,7 @@ static volatile unsigned char radio_is_on = 0;
 #define LEDS_ON(x) leds_on(x)
 #define LEDS_OFF(x) leds_off(x)
 #define LEDS_TOGGLE(x) leds_toggle(x)
-#define DEBUG 0
+#define DEBUG 1
 #if DEBUG
 #include <stdio.h>
 #define PRINTF(...) printf(__VA_ARGS__)
@@ -1382,30 +1382,45 @@ input_packet(void)
 		uint8_t ack[MAX_STROBE_SIZE];
 		uint8_t ack_len, len;
 		linkaddr_t temp;
-		// Copying original packetbuf
-		  packet = queuebuf_new_from_packetbuf();
-
 #if DUAL_RADIO
 #if LSA_MAC
+		// Copying original packetbuf
+//		packetbuf_compact();
+		packet = queuebuf_new_from_packetbuf();
 
-
-		if(linkaddr_cmp(packetbuf_addr(PACKETBUF_ADDR_RECEIVER),&linkaddr_node_addr) == 1){
+		if (radio_received_is_longrange()==LONG_RADIO){
+			dual_radio_switch(LONG_RADIO);
+		}	else if (radio_received_is_longrange() == SHORT_RADIO){
+			dual_radio_switch(SHORT_RADIO);
+		}
+/*		if(linkaddr_cmp(packetbuf_addr(PACKETBUF_ADDR_RECEIVER),&linkaddr_node_addr) == 1){
 			for_short = 1;
+			dual_radio_switch(SHORT_RADIO);
 		} else if(linkaddr_cmp(packetbuf_addr(PACKETBUF_ADDR_RECEIVER),&long_linkaddr_node_addr) == 1) {
 			for_short = 0;
-		}
+			dual_radio_switch(LONG_RADIO);
+		}*/
 #endif
 #endif
 		linkaddr_copy(&temp,packetbuf_addr(PACKETBUF_ADDR_SENDER));
 		packetbuf_clear();
-		packetbuf_set_addr(PACKETBUF_ADDR_RECEIVER,
-				&temp);
+		packetbuf_set_addr(PACKETBUF_ADDR_RECEIVER,&temp);
 #if DUAL_RADIO
-		if(sending_in_LR() == LONG_RADIO){
+/*		if(for_short == 0){
 			target = LONG_RADIO;
+			dual_radio_switch(target);
 			packetbuf_set_addr(PACKETBUF_ADDR_SENDER, &long_linkaddr_node_addr);
 		}	else	{
 			target = SHORT_RADIO;
+			dual_radio_switch(target);
+			packetbuf_set_addr(PACKETBUF_ADDR_SENDER, &linkaddr_node_addr);
+		}*/
+		if(sending_in_LR() == LONG_RADIO)
+		{
+			packetbuf_set_addr(PACKETBUF_ADDR_SENDER, &long_linkaddr_node_addr);
+		}
+		else
+		{
 			packetbuf_set_addr(PACKETBUF_ADDR_SENDER, &linkaddr_node_addr);
 		}
 #else
@@ -1423,6 +1438,7 @@ input_packet(void)
 //		packetbuf_compact();
 //		if(NETSTACK_FRAMER.create() >= 0) {
 
+/*
 #if DUAL_RADIO
 #if LSA_MAC
 			dual_radio_off(BOTH_RADIO);
@@ -1439,6 +1455,7 @@ input_packet(void)
 #else
 			on();
 #endif
+*/
 			NETSTACK_RADIO.send(ack, ack_len);
 			PRINTDEBUG("cxmac: send data ack %u\n", ack_len);
 //		} else {
